d8277a5565c37d860c13235952440e5777df2a98
Aditya Raj <aadtyaraj01@gmail.com>
2026-03-28 14:15:54 +0530

    Fix #42

--- kubernetes/k8s-exercises/01/tests.sh
+++ kubernetes/k8s-exercises/01/tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-if [[ $(kubectl get pod echo-pod --namespace default -o jsonpath='{.spec.restartPolicy}') == 'OnFailure' ]]; then
-	echo "01 passed"
-fi
-
+for filename in ./*
+	do cat $filename
+done
